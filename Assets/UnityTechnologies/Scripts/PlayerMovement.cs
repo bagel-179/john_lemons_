3a777666495ce2b2336ff9ef7d9a4220821493c0
bagel-179 <annakg9@gmail.com>
2023-12-09 02:45:11 -0500

    (almost) finished john lemons game

--- Assets/UnityTechnologies/Scripts/PlayerMovement.cs
+++ Assets/UnityTechnologies/Scripts/PlayerMovement.cs
@@ -1,25 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float turnSpeed = 20f;
+    private float playerWalkSpeed = 3f;
+    public float playerSprintSpeed = 5f;
+    public float normalSpeed = 3f;
 
     Animator m_Animator;
     Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
 
-    // Start is called before the first frame update
     void Start()
     {
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
+        exhausted = false;
     }
-
-    // Update is called once per frame
-    void FixedUpdate ()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
@@ -32,15 +37,56 @@ public class PlayerMovement : MonoBehaviour
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool("IsWalking", isWalking);
 
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
+
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
+
+        UpdateSpeed();
+        Update();
+    }
+    bool exhausted = false;
+    float sprintTime;
+    float exhaustTime;
+    void UpdateSpeed()
+    {
+        if (!exhausted && Input.GetKey(KeyCode.Space))
+        {
+            sprintTime += Time.deltaTime;
+            playerWalkSpeed = playerSprintSpeed;
+        }
+        else
+        {
+            playerWalkSpeed = normalSpeed;
+        }
     }
 
+    void Update()
+    {
+        if (sprintTime >= 4f)
+            exhausted = true;
+        if (exhausted == true)
+            exhaustTime += Time.deltaTime;
+        if (exhaustTime >= 5f)
+        {
+            sprintTime = 0f;
+            exhaustTime = 0f;
+            exhausted = false;
+        }
+    }
     void OnAnimatorMove()
     {
-        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MovePosition(m_Rigidbody.position + (m_Movement * playerWalkSpeed) * Time.deltaTime);
         m_Rigidbody.MoveRotation(m_Rotation);
-
     }
-
 }
